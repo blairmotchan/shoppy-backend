481a433653d386d0bcc2e3e445666d04c5ca0b13
Blair Motchan <blair.motchan@powerreviews.com>
2024-11-18 12:23:48 -0500

    users stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerModule } from 'nestjs-pino';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
   imports: [
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configservice: ConfigService) => {
+        return {
+          dialect: 'postgres',
+          host: configservice.get('DATABASE_HOST'),
+          port: 5432,
+          username: configservice.get('DATABASE_USERNAME'),
+          password: configservice.get('DATABASE_PASSWORD'),
+          database: configservice.get('DATABASE'),
+          models: [],
+        };
+      },
+    }),
     LoggerModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => {
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserRequest } from './dto/create-user.request';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from './model/user.model';
 
 @Injectable()
 export class UsersService {
+  constructor(@InjectModel(User) private userModel: typeof User) {}
+
   createUser(data: CreateUserRequest) {}
+
+  async findAll(): Promise<User[]> {
+    return this.userModel.findAll();
+  }
+
+  async findOne(id: string): Promise<User> {
+    return this.userModel.findOne({ where: { id } });
+  }
+
+  async remove(id: string): Promise<void> {
+    const user = await this.findOne(id);
+    await user.destroy();
+  }
 }
